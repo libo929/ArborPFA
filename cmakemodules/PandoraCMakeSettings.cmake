509ecd27b515f120b3e40cbbf83c78b1a8dcc46c
Bo Li <bo.li@llr.in2p3.fr>
2017-08-11 15:28:14 +0200

    deleted cmakemodules

--- cmakemodules/PandoraCMakeSettings.cmake
+++
@@ -1,8 +0,0 @@
-include(MacroPandoraDisplayStandardVariables)
-include(MacroPandoraGeneratePackageConfigFiles)
-
-include(PandoraDefaultInstallPrefix)
-include(PandoraDefaultBuildType)
-include(PandoraDefaultLibraryVersioning)
-include(PandoraDefaultBuildOutputDirectories)
-include(PandoraDefaultRPathSettings)
